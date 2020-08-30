3eb163380830501dd0bf66464a53ddfc67436dc9
xxx <xxx@xxx.com>
2020-08-30 20:32:46 +0200

    Implement switch player at each round

--- app/src/main/java/com/dev/tictactoe/game/TicTacToe.kt
+++ app/src/main/java/com/dev/tictactoe/game/TicTacToe.kt
@@ -5,4 +5,15 @@ import com.dev.tictactoe.player.Player
 class TicTacToe {
 
     var currentPlayer: Player = Player.X
+
+    fun goToNextRound() {
+        currentPlayer = when (currentPlayer) {
+            is Player.X -> Player.O
+            is Player.O -> Player.X
+        }
+    }
+
+    companion object {
+        const val BOARD_SIZE = 3
+    }
 }
--- app/src/test/java/com/dev/tictactoe/TicTacToeRuleTest.kt
+++ app/src/test/java/com/dev/tictactoe/TicTacToeRuleTest.kt
@@ -2,6 +2,7 @@ package com.dev.tictactoe
 
 import com.dev.tictactoe.game.TicTacToe
 import com.dev.tictactoe.player.Player
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class TicTacToeRuleTest {
@@ -11,5 +12,16 @@ class TicTacToeRuleTest {
         val game = TicTacToe()
         val currentPlayer = game.currentPlayer
         assert(currentPlayer is Player.X)
+    }
+
+    @Test
+    fun testSwitchPlayerAtEachRound() {
+        val game = TicTacToe()
+        val maxRound = TicTacToe.BOARD_SIZE * TicTacToe.BOARD_SIZE
+
+        for (round in 0 until maxRound) {
+            assertThat(game.currentPlayer).isInstanceOf(if (round % 2 == 0) Player.X::class.java else Player.O::class.java)
+            game.goToNextRound()
+        }
     }
 }
